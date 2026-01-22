6e76fe24359ac27b6a7a3ffeaf03312053f02d17
Jaeden Thia <jaeden@192.168.1.19>
2026-01-22 22:11:34 +0800

    Add list and add function

--- src/main/java/Tika.java
+++ src/main/java/Tika.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Tika {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int count = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Tika");
@@ -12,12 +14,31 @@ public class Tika {
         String input = scanner.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
-            input = scanner.nextLine();
+            switch (input) {
+                case "list":
+                    if (count == 0) {
+                        System.out.println("____________________________________________________________");
+                        System.out.println("List is empty... :(");
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        System.out.println("____________________________________________________________");
+                        for (int i = 1; i <= count; i++) {
+                            System.out.println(i + ". " + list[i - 1]);
+                        }
+                        System.out.println("____________________________________________________________");
+                    }
+                    input = scanner.nextLine();
+                    break;
+                default:
+                    System.out.println("____________________________________________________________");
+                    list[count] = input;
+                    count++;
+                    System.out.println("added: " + input);
+                    System.out.println("____________________________________________________________");
+                    input = scanner.nextLine();
+                    break;
+            }
         }
-
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
