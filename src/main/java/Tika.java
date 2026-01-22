0fd3f93d7503fc5ca149df5c23806bbcae6e770f
Jaeden Thia <jaeden@192.168.1.19>
2026-01-22 19:06:18 +0800

    Add echo

--- src/main/java/Tika.java
+++ src/main/java/Tika.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Tika {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Tika");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+            input = scanner.nextLine();
+        }
+
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
