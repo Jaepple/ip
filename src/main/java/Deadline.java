e6bb2cc8e4f007dffcfae602bf0d9f9dd5d9e4d4
jaeden <jaedenthia22@gmail.com>
2026-01-28 18:04:33 +0800

    Add java date and time feature

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,25 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String deadline;
+    private final LocalDateTime deadline;
+    private static final DateTimeFormatter INPUT_FORMAT =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMAT =
+            DateTimeFormatter.ofPattern("MMM dd yyyy h:mm a");
 
     public Deadline(String description, String deadline) {
         super(description);
-        this.deadline = deadline;
+        this.deadline = LocalDateTime.parse(deadline, INPUT_FORMAT);
     }
 
     @Override
     public String toString() {
-        return "[D][" + this.getStatusIcon() + "] " + this.description + " (by: " + this.deadline + ")";
+        return "[D][" + this.getStatusIcon() + "] " + this.description + " (by: " + this.deadline.format(OUTPUT_FORMAT) + ")";
     }
 
     @Override
     public String toFileString() {
-        return "D | " + (this.isDone ? "1" : "0") + " | " + this.description + " | " + this.deadline;
+        return "D | " + (this.isDone ? "1" : "0") + " | " + this.description + " | " + this.deadline.format(INPUT_FORMAT);
     }
 }
