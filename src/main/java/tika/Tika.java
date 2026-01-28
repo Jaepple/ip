19e9705746d19e8c8606cb139b636e28962d776f
jaeden <jaedenthia22@gmail.com>
2026-01-29 03:29:35 +0800

    Configure code style intellij

--- src/main/java/tika/Tika.java
+++ src/main/java/tika/Tika.java
@@ -1,6 +1,7 @@
 package tika;
 
 public class Tika {
+
     public static void main(String[] args) {
         Ui ui = new Ui();
         Storage storage = new Storage();
@@ -12,14 +13,18 @@ public class Tika {
         while (!input.equals("bye")) {
             try {
                 String firstWord = Parser.getCommandWord(input);
+
                 if (Parser.length(input) == 2) {
                     try {
                         int number = Parser.parseIndex(input);
-                        if (number > taskList.size()) { // to fix: should allow todo 3 as a task
+                        if (number > taskList.size()) {
                             throw new TikaException("Not a valid task!");
                         }
+
                         Task currTask = taskList.get(number - 1);
-                        if (firstWord.equals("mark")) {
+
+                        switch (firstWord) {
+                        case "mark":
                             if (currTask.isDone) {
                                 throw new TikaException("Task is already marked.");
                             }
@@ -28,7 +33,8 @@ public class Tika {
                             ui.markTask(currTask);
                             input = ui.readCommand();
                             continue;
-                        } else if (firstWord.equals("unmark")) {
+
+                        case "unmark":
                             if (!currTask.isDone) {
                                 throw new TikaException("Task is already unmarked.");
                             }
@@ -37,47 +43,55 @@ public class Tika {
                             ui.unmarkTask(currTask);
                             input = ui.readCommand();
                             continue;
-                        } else if (firstWord.equals("delete")) {
+
+                        case "delete":
                             storage.save(taskList.getTasks());
                             taskList.remove(number - 1);
                             ui.deleteTask(currTask, taskList.size());
                             input = ui.readCommand();
                             continue;
+
+                        default:
+                            break;
                         }
-                    } catch (NumberFormatException e) {
-                        System.out.println("plop");
+                    } catch (NumberFormatException ignored) {
                     }
                 }
 
                 switch (firstWord) {
-                    case "list":
-                        if (taskList.size() == 0) {
-                            throw new TikaException("List is empty... :(");
-                        } else {
-                            ui.showLine();
-                            for (int i = 1; i <= taskList.size(); i++) {
-                                Task currTask = taskList.get(i - 1);
-                                System.out.println(i + "." + currTask.toString());
-                            }
-                            ui.showLine();
-                        }
-                        input = ui.readCommand();
-                        break;
-                    case "todo", "deadline", "event":
-                        Task newTask = Parser.parseTask(input);
-                        taskList.add(newTask);
-                        storage.save(taskList.getTasks());
-                        ui.addTask(newTask, taskList.size());
-                        input = ui.readCommand();
-                        break;
-                    default:
-                        throw new TikaException("Not a valid task type! Try again.");
+                case "list":
+                    if (taskList.size() == 0) {
+                        throw new TikaException("List is empty... :(");
+                    }
+                    ui.showLine();
+                    for (int i = 1; i <= taskList.size(); i++) {
+                        Task currTask = taskList.get(i - 1);
+                        System.out.println(i + "." + currTask.toString());
+                    }
+                    ui.showLine();
+                    input = ui.readCommand();
+                    break;
+
+                case "todo":
+                case "deadline":
+                case "event":
+                    Task newTask = Parser.parseTask(input);
+                    taskList.add(newTask);
+                    storage.save(taskList.getTasks());
+                    ui.addTask(newTask, taskList.size());
+                    input = ui.readCommand();
+                    break;
+
+                default:
+                    throw new TikaException("Not a valid task type! Try again.");
                 }
+
             } catch (TikaException e) {
                 ui.showMessage(e.getMessage());
                 input = ui.readCommand();
             }
         }
+
         ui.showBye();
     }
 }
