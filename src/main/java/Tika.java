628d328b2880f46d2dc31e6320375a201fe65c3d
jaeden <jaedenthia22@gmail.com>
2026-01-28 23:55:31 +0800

    Add Parser class

--- src/main/java/Tika.java
+++ src/main/java/Tika.java
@@ -12,16 +12,15 @@ public class Tika {
 
         while (!input.equals("bye")) {
             try {
-                String[] parts = input.split(" ");
-                String firstWord = parts[0];
-                if (parts.length == 2) {
+                String firstWord = Parser.getCommandWord(input);
+                if (Parser.length(input) == 2) {
                     try {
-                        int number = Integer.parseInt(parts[1]);
+                        int number = Parser.parseIndex(input);
                         if (number > taskList.size()) { // to fix: should allow todo 3 as a task
                             throw new TikaException("Not a valid task!");
                         }
                         Task currTask = taskList.get(number - 1);
-                        if (parts[0].equals("mark")) {
+                        if (firstWord.equals("mark")) {
                             if (currTask.isDone) {
                                 throw new TikaException("Task is already marked.");
                             }
@@ -30,7 +29,7 @@ public class Tika {
                             ui.markTask(currTask);
                             input = ui.readCommand();
                             continue;
-                        } else if (parts[0].equals("unmark")) {
+                        } else if (firstWord.equals("unmark")) {
                             if (!currTask.isDone) {
                                 throw new TikaException("Task is already unmarked.");
                             }
@@ -39,7 +38,7 @@ public class Tika {
                             ui.unmarkTask(currTask);
                             input = ui.readCommand();
                             continue;
-                        } else if (parts[0].equals("delete")) {
+                        } else if (firstWord.equals("delete")) {
                             storage.save(taskList.getTasks());
                             taskList.remove(number - 1);
                             ui.deleteTask(currTask, taskList.size());
@@ -66,54 +65,8 @@ public class Tika {
                         input = ui.readCommand();
                         break;
                     case "todo", "deadline", "event":
-                        Task newTask;
-                        String description;
-                        switch (firstWord) {
-                            case "todo":
-                                if (input.substring("todo".length()).isEmpty()) {
-                                    throw new TikaException("The description of a todo cannot be empty.");
-                                }
-                                description = input.substring("todo ".length());
-                                newTask = new ToDo(description);
-                                taskList.add(newTask);
-                                break;
-                            case "deadline":
-                                if (input.substring("deadline".length()).isEmpty()) {
-                                    throw new TikaException("The description of a deadline cannot be empty.");
-                                }
-                                try {
-                                    String[] split = input.split(" /by ");
-                                    if (split.length < 2) {
-                                        throw new TikaException(
-                                                "Deadline must be: deadline <desc> /by yyyy-mm-dd HHmm"
-                                        );
-                                    }
-
-                                    description = split[0].substring("deadline ".length());
-                                    newTask = new Deadline(description, split[1]);
-                                    taskList.add(newTask);
-                                    break;
-
-                                } catch (DateTimeParseException e) {
-                                    throw new TikaException(
-                                            "Invalid date/time format! Use yyyy-mm-dd HHmm"
-                                    );
-                                }
-                            case "event":
-                                if (input.substring("event".length()).isEmpty()) {
-                                    throw new TikaException("The description of a event cannot be empty.");
-                                }
-                                String[] split1 = input.split(" /from ");
-                                // do a check for /from present
-                                description = split1[0].substring("event ".length());
-                                String[] split2 = split1[1].split(" /to ");
-                                // do a check for /to present
-                                newTask = new Event(description, split2[0], split2[1]);
-                                taskList.add(newTask);
-                                break;
-                            default:
-                                throw new IllegalStateException("Unexpected value: " + firstWord);
-                        }
+                        Task newTask = Parser.parseTask(input);
+                        taskList.add(newTask);
                         storage.save(taskList.getTasks());
                         ui.addTask(newTask, taskList.size());
                         input = ui.readCommand();
